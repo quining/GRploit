e0ef5a8e35cfd05a3c8a9cfc2aadb407b5188ca8
marios_118 <108134871+marios118@users.noreply.github.com>
2024-08-06 18:32:58 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,7 +4,7 @@ local loader = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local UICorner = Instance.new("UICorner")
 local grploits = Instance.new("TextLabel")
-local TextLabel = Instance.new("TextLabel")
+local test = Instance.new("TextLabel")
 
 --Properties:
 
@@ -16,8 +16,8 @@ Frame.Parent = loader
 Frame.BackgroundColor3 = Color3.fromRGB(21, 21, 21)
 Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
 Frame.BorderSizePixel = 0
-Frame.Position = UDim2.new(0.263678998, 0, 0.247680888, 0)
-Frame.Size = UDim2.new(0, 907, 0, 543)
+Frame.Position = UDim2.new(0.778212309, 0, 0.815362871, 0)
+Frame.Size = UDim2.new(0, 425, 0, 182)
 
 UICorner.CornerRadius = UDim.new(0, 20)
 UICorner.Parent = Frame
@@ -28,8 +28,8 @@ grploits.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 grploits.BackgroundTransparency = 1.000
 grploits.BorderColor3 = Color3.fromRGB(0, 0, 0)
 grploits.BorderSizePixel = 0
-grploits.Position = UDim2.new(0.175303191, 0, 0.372686952, 0)
-grploits.Size = UDim2.new(0, 588, 0, 160)
+grploits.Position = UDim2.new(0.268847078, 0, 0.0886927992, 0)
+grploits.Size = UDim2.new(0, 221, 0, 65)
 grploits.Font = Enum.Font.SourceSansBold
 grploits.Text = "GRploits"
 grploits.TextColor3 = Color3.fromRGB(255, 255, 255)
@@ -37,54 +37,27 @@ grploits.TextScaled = true
 grploits.TextSize = 14.000
 grploits.TextWrapped = true
 
-TextLabel.Parent = Frame
-TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.BackgroundTransparency = 1.000
-TextLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel.BorderSizePixel = 0
-TextLabel.Position = UDim2.new(0.175303191, 0, 0.28797242, 0)
-TextLabel.Size = UDim2.new(0, 588, 0, 160)
-TextLabel.Font = Enum.Font.SourceSansBold
-TextLabel.Text = "loading"
-TextLabel.TextColor3 = Color3.fromRGB(255, 255, 255)
-TextLabel.TextSize = 41.000
-TextLabel.TextWrapped = true
+test.Name = "test"
+test.Parent = Frame
+test.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+test.BackgroundTransparency = 1.000
+test.BorderColor3 = Color3.fromRGB(0, 0, 0)
+test.BorderSizePixel = 0
+test.Position = UDim2.new(0.0900235549, 0, 0.335945547, 0)
+test.Size = UDim2.new(0, 348, 0, 59)
+test.Font = Enum.Font.SourceSansItalic
+test.Text = "Dev Version Loaded"
+test.TextColor3 = Color3.fromRGB(255, 255, 255)
+test.TextScaled = true
+test.TextSize = 14.000
+test.TextWrapped = true
 
 -- Scripts:
 
-local function ACMK_fake_script() -- Frame.LocalScript 
-	local script = Instance.new('LocalScript', Frame)
+local function ZRMFK_fake_script() -- Frame.Script 
+	local script = Instance.new('Script', Frame)
 
-	local frame = script.Parent
-	local textLabel = frame:WaitForChild("TextLabel")
-	
-	frame.BackgroundTransparency = 1
-	textLabel.TextTransparency = 1
-	frame.Visible = true
-	textLabel.Text = ""
-	
-	local function tweenProperty(object, propertyTable, time)
-		local tweenInfo = TweenInfo.new(time, Enum.EasingStyle.Sine, Enum.EasingDirection.Out)
-		local tween = game:GetService("TweenService"):Create(object, tweenInfo, propertyTable)
-		tween:Play()
-		tween.Completed:Wait()
-	end
-	
-	local function updateText(newText, delayTime)
-		tweenProperty(textLabel, {TextTransparency = 0}, 0.5) 
-		textLabel.Text = newText
-		wait(delayTime)
-		tweenProperty(textLabel, {TextTransparency = 1}, 0.5) 
-	end
-	
-	tweenProperty(frame, {BackgroundTransparency = 0}, 1) 
-	
-	updateText("Loading...", 2)
-	updateText("Loading assets...", 2)
-	updateText("Thank you for using!", 2)
-	
-	tweenProperty(frame, {BackgroundTransparency = 1}, 1) 
-	frame.Visible = false
-	
+	wait(3)
+	script.Parent.Parent.Frame.Visible = false	
 end
-coroutine.wrap(ACMK_fake_script)()
+coroutine.wrap(ZRMFK_fake_script)()
